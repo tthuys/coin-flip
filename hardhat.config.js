5be3626b8238ea00aff364effa521260b551a5ea
Phan Thanh Thúy <90078502+tthuys@users.noreply.github.com>
2024-09-06 18:44:29 +0700

    replace constract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,17 @@
-require('@nomiclabs/hardhat-ethers');
+import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-module.exports = {
+//0x10127BE7f11B9471AA8F7CB10186581FF0d42398
+
+const config: HardhatUserConfig = {
   solidity: "0.8.19",
-  networks: {
-    hardhat: {},
-    // Configure other networks here if needed
-  },
-  etherscan: {
-    // Your API key for etherscan if you want to verify contracts
-    apiKey: "YOUR_ETHERSCAN_API_KEY"
+  networks : {
+    sepolia : {
+      url: process.env.ALCHEMY_API,
+      accounts: [process.env.PRIVATE_KEY !== undefined ? process.env.PRIVATE_KEY: '']
+    }
   }
 };
+
+export default config;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,42 @@
-const { ethers, network } = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
 
-    const VRFExample = await ethers.getContractFactory("VRFExample");
-    const vrfCoordinator = "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B";
-    const keyHash = "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae";
-    const subscriptionId = 1;
+  const Random = await ethers.getContractFactory("Random");
+  const random = await Random.deploy();
+  await random.deployed();
 
-    const vrfExample = await VRFExample.deploy(vrfCoordinator, keyHash, subscriptionId);
-    console.log("VRFExample deployed to:", vrfExample.address);
+  console.log(
+    `Smart contract deployed to ${random.address}`
+  );
+  
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
+
+
+
+// const { ethers, network } = require("hardhat");
+
+// async function main() {
+//     const [deployer] = await ethers.getSigners();
+//     console.log("Deploying contracts with the account:", deployer.address);
+
+//     const VRFExample = await ethers.getContractFactory("VRFExample");
+//     const vrfCoordinator = "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B";
+//     const keyHash = "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae";
+//     const subscriptionId = 1;
+
+//     const vrfExample = await VRFExample.deploy(vrfCoordinator, keyHash, subscriptionId);
+//     console.log("VRFExample deployed to:", vrfExample.address);
+// }
+
+// main().catch((error) => {
+//     console.error(error);
+//     process.exitCode = 1;
+// });
